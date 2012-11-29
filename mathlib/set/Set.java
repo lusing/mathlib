1287d1bb45323344e1be103e8691b0456072e407
Louis <lusing@gmail.com>
2012-11-29 23:33:22 +0800

    Add Set equal algorithm

--- mathlib/set/Set.java
+++ mathlib/set/Set.java
@@ -9,4 +9,15 @@ public abstract class Set {
 	
 	public abstract boolean isIn(Element e);
 	public abstract boolean isSubset(Set b);
+	
+	/*
+	 * If A is in B and B is in A, then A=B
+	 */
+	public boolean isEqual(Set b){
+		return (this.isSubset(b) && b.isSubset(this));
+	}
+	
+	public static boolean isIn(Element e, Set a){
+		return a.isIn(e);
+	}
 }
