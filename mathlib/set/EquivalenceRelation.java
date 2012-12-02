780c2278e01c5f39cb199cf51b58ea165871c5df
Louis <lusing@gmail.com>
2012-12-02 17:20:38 +0800

    Add EquivalenceRelation class

---
+++ mathlib/set/EquivalenceRelation.java
@@ -0,0 +1,13 @@
+package mathlib.set;
+
+public class EquivalenceRelation extends BinaryRelation {
+	public boolean isReflexivity(){
+		return true;
+	}
+	public boolean isSymmetry(){
+		return true;
+	}
+	public boolean isTransitivity(){
+		return true;
+	}
+}
